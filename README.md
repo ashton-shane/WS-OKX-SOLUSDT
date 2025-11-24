901da0b86ebeccae71dd66bd24e6c6c23b1b8d4a
Ashton Shane <ashton.shane.dev@gmail.com>
2025-11-24 21:06:35 +0800

    add ws boilerplate for server

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WS-OKX-SOLUSDT
-A backend project implementing websockets to determine using two sessions which session is faster. 
+A backend project implementing websockets in Python together with the OKX Websocket API for SOL-USDT trades.
+
+TASK: Using two sessions, determine how many times one session returns more quickly compared to the other. 
+
---
+++ server.py
@@ -0,0 +1,15 @@
+import asyncio
+import websockets
+from websockets.asyncio.server import serve
+
+async def handler(websocket):
+    while True:
+        message = await websocket.recv()
+        print(message)
+        
+async def main():
+    async with serve(handler, "", 8001) as server:
+        await server.serve_forever()
+
+if __name__ == "__main__":
+    asyncio.run(main())
