b05175aab1340060c7f4feaea9954342268433ed
Ashton Shane <ashton.shane.dev@gmail.com>
2025-11-29 17:58:58 +0800

    create separate function for number of connections

--- client.py
+++ client.py
@@ -1,27 +1,35 @@
 import asyncio
-from helpers import get_conn_period, get_winner, get_trades, create_task_list
+from helpers import get_conn_period, get_winner, get_trades, create_task_list, get_num_conn
 
 async def main():
-    conn_period = get_conn_period()
     print("\n================================================================================\n")
+    
+    # Get period of connection
+    conn_period = get_conn_period()
+    # Get number of parallel connections
+    n = get_num_conn()
 
     # Create an async queue to push timestamps
     queue = asyncio.Queue()
 
     # Dynamically create concurrent websocket sessions to the same channel, push to a list
-    tasks = create_task_list(queue, conn_period)
+    tasks = create_task_list(queue, conn_period, n)
     
     # Need to eventually await the list of tasks
-    await asyncio.gather(*tasks) # * it is the splat operator that splits into separate tasks.=
-
+    await asyncio.gather(*tasks) # * it is the splat operator that splits into separate tasks.
+    
+    # Add an end signaller for iteration later
+    await queue.put(None)
+    
     # get total connections to confirm
     print("\n------------------------ RESULTS -------------------------\n") # take away the first and last response (confirmation + signaller)
 
+    # Note that the first response is always a confirmation of what you sent, followed by the actual data
     # Remove from queues the first timestamp as that is the confirmation response
     await queue.get()
-
+    
     # Tabulate scores and get winner
-    await get_winner(queue)
+    await get_winner(queue, n)
 
 
 if __name__ == "__main__":
--- helpers.py
+++ helpers.py
@@ -2,6 +2,17 @@ import json
 import time
 from websockets import connect
 import asyncio
+from collections import deque
+
+def get_conn_period():
+    while True:
+        try:
+            secs = int(input("Period (in seconds) to run websocket: ").strip())
+            if isinstance(secs, int):
+                return secs
+        except ValueError:
+            print("ERROR: Please only input an integer value")
+
 
 async def get_trades(queue, conn, secs):
     # Initialise the required params for the API
@@ -22,6 +33,7 @@ async def get_trades(queue, conn, secs):
     async with connect(uri) as websocket:
         # Send params to server
         await websocket.send(json.dumps(req_params)) 
+        
         # Don't process the first websocket response
         await websocket.recv()
 
@@ -32,75 +44,51 @@ async def get_trades(queue, conn, secs):
         # While loop to keep the data flowing until cancelled
         print(f"Connection {conn} receiving websocket responses from OKX...")
         while end > time.perf_counter():
-            # Note that the first response is always a confirmation of what you sent, followed by the actual data
+            
             recv_data = json.loads(await websocket.recv())
-            now = time.time() * 1000       # in miliseconds
+            now = time.time() * 1000                                    # in miliseconds
             latency = now - (float(recv_data["data"][0]["ts"]))
-            print(f"Latency is {latency}")
-            await queue.put(latency)       # push timestamp into queue with each response
+            tradeId = recv_data["data"][0]["tradeId"]
+            obj = {
+                "connection": conn,
+                "tradeId": tradeId,
+                "latency": latency
+            }
+            print(obj)
+            await queue.put(obj)       # push timestamp into queue with each response
         print(f"...connection {conn} responses stopped.")
 
-        # Add an end signaller for iteration later
-        await queue.put(None)
-
-def get_conn_period():
-    while True:
-        try:
-            secs = int(input("Period (in seconds) to run websocket: ").strip())
-            if isinstance(secs, int):
-                return secs
-        except ValueError:
-            print("ERROR: Please only input an integer value")
-
-
-def create_task_list(queue, conn_period):
+def get_num_conn():
     while True:
         try:
             n = int(input("How many connections do you wish to start: "))
             if isinstance(n, int):
-                break
+                return n
         except ValueError:
             print("ERROR: Please only input an integer value")
         
-        tasks = []
-        for i in range(n):
-            task = asyncio.create_task(get_trades(queue, f"{i}", conn_period))
-            tasks.append(task)
+def create_task_list(queue, conn_period, n):
+    task_list = []
 
-    return tasks
+    for i in range(n):
+        task = asyncio.create_task(get_trades(queue, f"{i+1}", conn_period))
+        task_list.append(task)
+    return task_list
 
-async def get_winner(queue_a, queue_b):
+
+async def get_winner(queue):
+    # create a trade ID queue, while the current trade ID is there, add to a hash table. 
+    # the moment trade ID changes, popleft, and add new tradeID to begin cycle.
     # create hash table to tabulate scores then iterate
-    scores = {"A" : 0, 
-              "B" : 0, 
-              "DRAW": 0}
-    
-    while True:
-        # Get each values - queues need to be awaited even when getting because they are channels
-        a, b = await asyncio.gather(queue_a.get(), queue_b.get())
+    arr = []
+    curr_trade_id = deque(arr)
 
-        # Check for end signaller, and add end scores if necessary
-        if not a and not b:
-            break
-        elif b and not a:
-            scores["B"] += 1    # means be managed to squeeze in one more connection before it closed
-        elif a and not b:
-            scores["A"] += 1
 
-        # populate hash table with wins
-        if a < b:
-            scores["A"] += 1
-        elif b < a:
-            scores["B"] += 1
-        else:
-            scores["DRAW"] += 1
+    scores = {}
 
-    # determine winner
-    most = max(scores["A"], scores["B"])
-    winner = next(k for k,v in scores.items() if scores[k] == most)
-    diff = abs(scores["A"] - scores["B"])
+    return
 
-    print(f"There were {scores['DRAW']} draws")
-    print(f"Connection A scored {scores["A"]} times while connection B scored {scores["B"]} times")
+    print(f"There were draws")
+    print(f"Connection A scored times while connection B scored {scores["B"]} times")
     print(f"Connection {winner} is the winner with {most} wins and was faster {diff} times!")
     print("\n================================================================================\n")
